0067a06e57ae01d143a3e1ec14f753c3c7ad8265
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2023-07-05 10:08:46 +0000

    Deploying to gh-pages from @ Loboda-Anna/goit-react-hw-05-movies@99392d873a027b5a090618eaef3b855a0001ea42 🚀

---
+++ static/js/130.af179799.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[130],{5130:function(e,t,r){r.r(t),r.d(t,{default:function(){return o}});r(2791);var n=r(7689),c=r(1087),i=r(184);function o(e){var t=e.movies,r=(0,n.TH)();return(0,i.jsx)("ul",{children:t.map((function(e){return(0,i.jsx)("li",{children:(0,i.jsx)(c.rU,{to:"movies/".concat(e.id),state:{from:r},children:e.title})},e.id)}))})}}}]);
+//# sourceMappingURL=130.af179799.chunk.js.map
--- static/js/130.c63db3b2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[130],{5130:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});r(2791);var n=r(7689),c=r(1087),i=r(184);function u(e){var t=e.movies,r=(0,n.TH)();return(0,i.jsx)("ul",{children:t.map((function(e){return(0,i.jsx)("li",{children:(0,i.jsx)(c.rU,{to:"".concat(e.id),state:{from:r},children:e.title})},e.id)}))})}}}]);
-//# sourceMappingURL=130.c63db3b2.chunk.js.map
